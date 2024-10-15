d22d1ca1f92c09ba0cf43b3f241cc7ab2d09f746
minou.matokonkalou <mtk14minou@gmail.com>
2024-10-15 21:23:08 +0200

    init of the project

---
+++ src/test/java/fr/quickstart/shopping_cart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.quickstart.shopping_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
